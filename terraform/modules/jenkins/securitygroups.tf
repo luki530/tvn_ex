d14fb7004c3ac20b81099312c2e2b13af0a19aa5
luki530 <lukasz.oszczypala@gmail.com>
2024-03-06 14:18:13 +0100

    open 80 for jenkins task

--- terraform/modules/jenkins/securitygroups.tf
+++ terraform/modules/jenkins/securitygroups.tf
@@ -20,6 +20,15 @@ resource "aws_security_group_rule" "ecs-egress-443" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
+resource "aws_security_group_rule" "ecs-egress-80" {
+  security_group_id = aws_security_group.jenkins-ecs.id
+  type              = "egress"
+  protocol          = "TCP"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
 resource "aws_security_group_rule" "ecs-egress-nfs" {
   security_group_id        = aws_security_group.jenkins-ecs.id
   type                     = "egress"
